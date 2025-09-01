942ecab3d69de5a9fc4dce43b47ce0e2734cbf86
Eline H <eline.haukanes@scaleaq.com>
2025-09-01 14:06:27 +0200

    Trying to trigger again

--- src/consumer-contract.pacttest.ts
+++ src/consumer-contract.pacttest.ts
@@ -33,7 +33,7 @@ const pact = new PactV4({
 })
 
 describe('Movies API', () => {
-  describe('When a GET request is made to /movies (test run trigger)', () => {
+  describe('When a GET request is made to /movies', () => {
     it('should return all movies', async () => {
       const EXPECTED_BODY: Movie = {
         id: 1,
@@ -52,7 +52,7 @@ describe('Movies API', () => {
       await pact
         .addInteraction()
         .given(stateName, stateParams)
-        .uponReceiving('a request to get all movies')
+        .uponReceiving('a request to get all movies, TRIGGER')
         .withRequest('GET', '/movies')
         .willRespondWith(
           200,
